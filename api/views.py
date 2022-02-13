31ab274506d3beeb81f84d24e43f69bb1c8ca248
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-13 18:34:16 +0100

    crud operations

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from .serializers import *
+from .models import *
 
-# Create your views here.
+class CreateGnome(generics.CreateAPIView):  
+    queryset = Gnome.objects.all()
+    serializer_class = GnomeSerializer
+    
+class GetGnome(generics.ListAPIView):
+    queryset = Gnome.objects.all()
+    serializer_class = GnomeSerializer
+
+class UpdateGnome(generics.RetrieveUpdateAPIView):
+    queryset = Gnome.objects.all()
+    serializer_class = GnomeSerializer
+
+class DeleteGnome(generics.DestroyAPIView):
+    queryset = Gnome.objects.all()
+    serializer_class = GnomeSerializer
