b54f1def8f6670391e22eb75dd4b9532216f94c1
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-13 16:30:25 +0100

    Database layout

--- api/models.py
+++ api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+class Gnome(models.Model):
+
+    class Races(models.TextChoices):
+        ROCK = 'Rock',_('Rock')
+        SKY = 'Sky',_('Sky')
+        FOREST = 'Forest',_('Forest')
+        FIRE = 'Fire',_('Fire')
+        RIVER = 'River',_('River')
+
+    race = models.CharField(
+        max_length=6,
+        choices=Races.choices,
+        default=Races.FOREST,
+    )
+
+    name = models.TextField(max_length=35)
+    age = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+
+    strength = models.PositiveSmallIntegerField(
+        validators=[
+            MaxValueValidator(150),
+            MinValueValidator(1)
+        ]
+    )
+    avatar = models.ImageField()
+
+    def __str__(self):
+        return self.name
