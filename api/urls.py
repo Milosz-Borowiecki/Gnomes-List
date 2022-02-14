2f9924db1b5446c0c4de60c7bc4137bc4ecc4a56
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-02-14 01:35:15 +0100

    Fix the Update endpoint path

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('',GetGnome.as_view()),
     path('create',CreateGnome.as_view()),
-    path('<int:pk>/',UpdateGnome.as_view()),
+    path('<int:pk>',UpdateGnome.as_view()),
     path('delete/<int:pk>',DeleteGnome.as_view())
 ]
